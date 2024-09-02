ea622da8ec15c537d9c717c0efa9783ce8f027e5
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-09-02 17:46:11 -0300

    Estrutura do projeto Pastas App e Grandle

--- AppListaCurso/app/build.gradle.kts
+++ AppListaCurso/app/build.gradle.kts
@@ -12,8 +12,6 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -37,7 +35,4 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
 }
