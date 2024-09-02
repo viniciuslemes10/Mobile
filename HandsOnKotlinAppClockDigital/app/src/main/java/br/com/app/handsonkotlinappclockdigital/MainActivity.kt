0003915b859ecb28e83d85fc498042b504b5f62b
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-09-02 15:20:49 -0300

    feat: implementando lógica para atualizar hora atual

--- HandsOnKotlinAppClockDigital/app/src/main/java/br/com/app/handsonkotlinappclockdigital/MainActivity.kt
+++ HandsOnKotlinAppClockDigital/app/src/main/java/br/com/app/handsonkotlinappclockdigital/MainActivity.kt
@@ -1,12 +1,23 @@
 package br.com.app.handsonkotlinappclockdigital
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextClock
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var txtHoraAtualizada: TextView? = null
+    private var txtHora24_Atual: TextClock? = null
+    private var txtHoraAtual: TextClock? = null
+    private var btnAtualizaHora: Button? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +27,21 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        iniciarComponentesDeLayout()
+    }
+
+    fun atualizarHoraAtual(view: View) {
+        txtHoraAtualizada!!.setText("Hora: " + txtHora24_Atual!!.getText())
+    }
+
+    private fun iniciarComponentesDeLayout() {
+        txtHoraAtual = findViewById(R.id.txtHoraAtual)
+        txtHora24_Atual = findViewById(R.id.txtHora24_Atual)
+        txtHoraAtualizada = findViewById(R.id.txtHoraAtualizada)
+        btnAtualizaHora = findViewById(R.id.btnAtualizaHora)
+
+        txtHoraAtual?.timeZone = "America/Sao_Paulo"
+        txtHora24_Atual?.timeZone = "America/Sao_Paulo"
     }
 }
