b3231b51eb1414ea2d3c357c8ce83920b595a3a9
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-09-02 14:17:04 -0300

    feat: implementando captura de hora

--- HandsOnJavaAppClockDigital/app/src/main/java/br/com/app/handsonjavaappclockdigital/MainActivity.java
+++ HandsOnJavaAppClockDigital/app/src/main/java/br/com/app/handsonjavaappclockdigital/MainActivity.java
@@ -1,6 +1,7 @@
 package br.com.app.handsonjavaappclockdigital;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextClock;
 import android.widget.TextView;
@@ -10,6 +11,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,7 +32,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         iniciarComponentesDeLayout();
+
+        capturarHoraAtual();
+    }
+
+    private void capturarHoraAtual() {
+
+        btnUpdateHoraAtual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                txtUpdateHoraAtual.setText("Hora: "+ txtHora24PM_AM.getText());
+            }
+        });
     }
 
     private void iniciarComponentesDeLayout() {
@@ -35,5 +54,8 @@ public class MainActivity extends AppCompatActivity {
         txtHoraAtual = findViewById(R.id.txtHoraAtual);
         txtHora24PM_AM = findViewById(R.id.txtHora24PM_AM);
         btnUpdateHoraAtual = findViewById(R.id.btnUpdateHoraAtual);
+
+        txtHora24PM_AM.setTimeZone("America/Sao_Paulo");
+        txtHoraAtual.setTimeZone("America/Sao_Paulo");
     }
 }
