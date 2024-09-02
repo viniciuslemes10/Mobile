4dcef08031a49a4ed60b4df0b0386fa3dc12f6ee
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-09-02 13:50:41 -0300

    feat: iniciando componentes do layout

--- HandsOnJavaAppClockDigital/app/src/main/java/br/com/app/handsonjavaappclockdigital/MainActivity.java
+++ HandsOnJavaAppClockDigital/app/src/main/java/br/com/app/handsonjavaappclockdigital/MainActivity.java
@@ -1,6 +1,9 @@
 package br.com.app.handsonjavaappclockdigital;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextClock;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtUpdateHoraAtual;
+    private TextClock txtHoraAtual, txtHora24PM_AM;
+    private Button btnUpdateHoraAtual;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        iniciarComponentesLayout();
+    }
+
+    private void iniciarComponentesLayout() {
+        txtUpdateHoraAtual.findViewById(R.id.txtUpdateHoraAtual);
+        txtHoraAtual.findViewById(R.id.txtHoraAtual);
+        txtHora24PM_AM.findViewById(R.id.txtHora24PM_AM);
+        btnUpdateHoraAtual.findViewById(R.id.btnUpdateHoraAtual);
     }
 }
